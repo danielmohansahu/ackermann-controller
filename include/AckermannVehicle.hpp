ce66857ae324380e9c8ad0675e33c2b7fbe2a8c8
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-07 19:09:49 -0400

    Modified Vehicle (removed positional and other unused variables)

--- include/AckermannVehicle.hpp
+++ include/AckermannVehicle.hpp
@@ -8,91 +8,43 @@
  * @copyright [2020]
  */
 
-#include <iostream>
-#include <Vehicle.hpp>
-#include <PIDController.hpp>
+#include "Vehicle.hpp"
+#include "State.hpp"
 
-extern const double global_dt;
+namespace ackermann
+{
 
-namespace VehicleControl {
+/* @brief This class is used to further define a vehicle with Ackermann steering
+ *
+ * This class implements a vehicle with Ackermann steering.
+ */
+class AckermannVehicle : public Vehicle
+{
+public:
+  // @brief constructor
+  AckermannVehicle(double maxSteeringAngle = 45.0);
 
-
-  /* @brief This class is used to further define a vehicle with Ackermann steering
-   *
-   * This class implements a vehicle with Ackermann steering.
+  /* @brief Set the maximum steering angle (degrees)
+   * @parameter maxAngle Maximum steering angle (degrees)
    */
-class AckermannVehicle : public Vehicle {
-  public:
-    // @brief constructor
-    AckermannVehicle(double maxSteeringAngle = 45., double maxSteeringRate = 10.);
+  void set_max_steering_angle(double maxAngle);
 
-    /* @brief Set the maximum steering angle (degrees)
-     * @parameter maxAngle Maximum steering angle (degrees)
-     */
-    void set_max_steering_angle(double maxAngle);
+  /* @brief Set the maximum steering angle (degrees)
+   * @parameter None
+   * @return Maximum steering angle (degrees) (double)
+   */
+  double get_max_steering_angle() const;
 
-    /* @brief Set the maximum steering angle rate of change (degrees/second)
-     * @parameter maxAngleRate Maximum steering angle rate (degrees/second)
-     */
-    void set_max_steering_rate(double maxAngleRate);
+  /* @brief Calculate Ackermann vehicle dynamics for vehicle
+   * @parameter headingChange Relative heading change for vehicle
+   * @parameter velocity Velocity of vehicle
+   * @return None
+   */
+  State getState(double speed, double heading) override;
 
-    /* @brief Set the current steering angle (degrees)
-     * @parameter currentSteeringAngle Current steering angle (degrees)
-     */
-    void set_current_steering_angle(double currentSteeringAngle);
-
-    /* @brief Set the maximum steering angle (degrees)
-     * @parameter None
-     * @return Maximum steering angle (degrees) (double)
-     */
-    double get_max_steering_angle() const;
-
-    /* @brief Set the maximum steering angle rate of change (degrees/second)
-     * @parameter None
-     * @return Maximum steering angle rate (degrees/second) (double)
-     */
-    double get_max_steering_rate() const;
-
-    /* @brief Get the current steering angle (degrees)
-     * @parameter None
-     * @return Current steering angle (degrees) (double)
-     */
-    double get_current_steering_angle() const;
-
-    /* @brief Calculate the speed of the left wheel on ground (ft/s)
-     * @parameter None
-     * @return Current speed of left wheel on ground (ft/s)
-     */
-    double CalcWheelSpeedLeft();
-
-    /* @brief Calculate the speed of the right wheel on ground
-     * @parameter None
-     * @return Current speed of right wheel on ground (ft/s)
-     */
-    double CalcWheelSpeedRight();
-
-
-    /* @brief STUB - Calculate Ackermann vehicle dynamics for vehicle
-     * @parameter headingChange Relative heading change for vehicle
-     * @parameter velocity Velocity of vehicle
-     * @return None
-     */
-    void compute(double headingChange, double velocity);
-
-    /* @brief Correct steering angle to maximum value and rates
-     * @parameter None
-     * @return None
-     */
-     void UpdateSteeringInput();
-
-  private:
-    // @brief Maximum steering angle
-    double max_steering_angle_;
-    // @brief Maximum steering rate
-    double max_steering_rate_;
-    // @brief Current steering angle
-    double current_steering_angle_;
-    // @brief Desired steering angle
-    double desired_steering_angle_;
+ private:
+  // @brief Maximum steering angle
+  double max_steering_angle_;
 };
-}
+
+} // namespace ackermann
