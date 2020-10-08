ac8d258a4c3529c386ad7ef16ef0815730b7e8bc
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-08 18:04:42 -0400

    Added State unit tests (really simple; it's just a struct)

--- test/unit/State.cpp
+++ test/unit/State.cpp
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
 #include <State.hpp>
 
-TEST(State, should_pass) {
-  EXPECT_EQ(1, 1);
+using ackermann::State;
+
+/* @brief check default construction of State */
+TEST(State_Construction, should_pass) {
+  State s;
+  EXPECT_EQ(s.left, 0.0);
+  EXPECT_EQ(s.right, 0.0);
 }
 
