991e8ca7b53e284cee91b75daa6f90f23620a389
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-08 18:41:15 -0400

    Added the simpler PID tests

--- test/unit/PID.cpp
+++ test/unit/PID.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <gtest/gtest.h>
 #include <PID.hpp>
 
@@ -5,12 +7,37 @@ using ackermann::PID;
 
 /* @brief Test all setters and getters. */
 TEST(PID_SettersAndGetters, should_pass) {
-  EXPECT_EQ(1, 1);
+  // initialize random values for kp/ki/kd
+  srand(time(NULL));
+  double kp = static_cast<double>(rand() / RAND_MAX);
+  double ki = static_cast<double>(rand() / RAND_MAX);
+  double kd = static_cast<double>(rand() / RAND_MAX);
+
+  // instantiate class
+  PID pid(kp, ki, kd);
+
+  // test getters
+  EXPECT_EQ(pid.get_k_p(), kp);
+  EXPECT_EQ(pid.get_k_i(), ki);
+  EXPECT_EQ(pid.get_k_d(), kd);
+
+  // test setters
+  pid.set_k_p(ki);
+  pid.set_k_i(kd);
+  pid.set_k_d(kp);
+  EXPECT_EQ(pid.get_k_p(), ki);
+  EXPECT_EQ(pid.get_k_i(), kd);
+  EXPECT_EQ(pid.get_k_d(), kp);
 }
 
 /* @brief Test compute command with default params.*/
 TEST(PID_Control1, should_pass) {
-  EXPECT_EQ(1, 1);
+  PID pid(0.0, 0.0, 0.0);
+
+  EXPECT_EQ(pid.compute(0.0, 0.0), 0.0);
+  EXPECT_EQ(pid.compute(1.0, 0.0), 0.0);
+  EXPECT_EQ(pid.compute(0.0, 1.0), 0.0);
+  EXPECT_EQ(pid.compute(1.0, 1.0), 0.0);
 }
 
 /* @brief Test compute command with known values. */
