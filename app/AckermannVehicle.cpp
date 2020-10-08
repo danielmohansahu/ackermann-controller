fce542e879e1881b0a1b3afbc95682852ada6317
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-07 20:44:13 -0400

    Added stub implementations of each class

--- app/AckermannVehicle.cpp
+++ app/AckermannVehicle.cpp
@@ -6,62 +6,36 @@
  * @author Daniel M. Sahu
  *
  * @copyright [2020]
+ * 
+ * http://www.iaeng.org/publication/WCE2016/WCE2016_pp1062-1066.pdf
+ * https://www.xarg.org/book/kinematics/ackerman-steering/
  */
 
 #include <AckermannVehicle.hpp>
 
-VehicleControl::AckermannVehicle::AckermannVehicle(double maxSteeringAngle,
-double maxSteeringRate)
-: max_steering_angle_{maxSteeringAngle},max_steering_rate_{maxSteeringRate}
-{
-  current_steering_angle_ = 0.;
-}
+// @TODO Currently STUB implementation; needs to be filled
 
-// stub implementation
-void VehicleControl::AckermannVehicle::compute(double headingChange, double velocity)
+namespace ackermann
 {
 
+AckermannVehicle::AckermannVehicle(double maxSteeringAngle)
+ : max_steering_angle_(maxSteeringAngled)
+{
 }
 
-// stub implementation
-double VehicleControl::AckermannVehicle::CalcWheelSpeedLeft() {
+void AckermannVehicle::set_max_steering_angle(double maxAngle)
+{
+  return;
+}
+
+double AckermannVehicle::get_max_steering_angle()
+{
   return 0.0;
 }
 
-// stub
-double VehicleControl::AckermannVehicle::CalcWheelSpeedRight() {
-  return 0.0;
+State AckermannVehicle::getState(double speed, double heading)
+{
+  return State();
 }
 
-void VehicleControl::AckermannVehicle::set_max_steering_angle(double newMaxAngle) {this->max_steering_angle_=newMaxAngle;}
-void VehicleControl::AckermannVehicle::set_max_steering_rate(double newMaxRate) {this->max_steering_rate_=newMaxRate;}
-void VehicleControl::AckermannVehicle::set_current_steering_angle(double currSteeringAngle) {this->current_steering_angle_ = currSteeringAngle;}
-
-double VehicleControl::AckermannVehicle::get_max_steering_angle() const {return this->max_steering_angle_;}
-double VehicleControl::AckermannVehicle::get_max_steering_rate() const {return this->max_steering_rate_;}
-double VehicleControl::AckermannVehicle::get_current_steering_angle() const {return this->current_steering_angle_;}
-
-void VehicleControl::AckermannVehicle::UpdateSteeringInput() {
-
-  double deltaSteeringAngleDesired = this->current_steering_angle_ - this->desired_steering_angle_;
-  double deltaSteeringAngleActual;
-
-  if (deltaSteeringAngleDesired >= (this->max_steering_rate_/global_dt)) {
-    deltaSteeringAngleActual = this->max_steering_rate_/global_dt;
-  }
-  else if (deltaSteeringAngleDesired <= ((-1*this->max_steering_rate_)/global_dt)) {
-    deltaSteeringAngleActual = this->max_steering_rate_/global_dt;
-  }
-  else {
-    deltaSteeringAngleActual = deltaSteeringAngleDesired;
-  }
-
-  double newAngle = this->current_steering_angle_ + deltaSteeringAngleActual;
-
-  if (newAngle >= this->max_steering_angle_) {this->current_steering_angle_ = this->max_steering_angle_;}
-  else if (newAngle <= (-1*this->max_steering_angle_)) {this->current_steering_angle_ = (-1*this->max_steering_angle_);}
-  else {this->current_steering_angle_ = newAngle;}
-
-}
-// http://www.iaeng.org/publication/WCE2016/WCE2016_pp1062-1066.pdf
-// https://www.xarg.org/book/kinematics/ackerman-steering/
+} // namespace ackermann
