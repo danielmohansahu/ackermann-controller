d9e975dbd27c51bec1d327325660b73a7c6cb783
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-08 19:02:10 -0400

    WIP adding Limits tests; this procedure made me rethink the Limit class API

--- test/unit/Limits.cpp
+++ test/unit/Limits.cpp
@@ -1,11 +1,23 @@
 #include <gtest/gtest.h>
 #include <Limits.hpp>
+#include <State.hpp>
 
 using ackermann::Limits;
+using ackermann::State;
 
 /* @brief Test limiting without any limits (should be default behavior).*/
 TEST(Limits_NoLimit, should_pass) {
-  EXPECT_EQ(1, 1);
+  double dt = 0.1;
+  Limits lim;
+
+  // instantiate some empty states to work with
+  State current;
+  State desired;
+
+  // check #1: we don't limit 0 moves
+  State actual = lim.limit(current, desired, dt);
+  EXPECT_EQ(desired.left, actual.left);
+  EXPECT_EQ(desired.right, actual.right);
 }
 
 /* @brief Test velocity limiting.*/
