5242275eefd106e5fd42fe90ee27daa66ecb3e7a
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-11 08:21:29 -0400

    [Task1.5] Using GTest Fixtures.
    [test/system.cpp] Ported global classes into a Test Fixture.

--- test/system.cpp
+++ test/system.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <gtest/gtest.h>
 
 #include <Controller.hpp>
@@ -13,74 +14,90 @@ using ackermann::Controller;
 // @TODO make targets random (with small range)
 // @TODO control until success is achieved (or timeout)
 
+
+/* @brief Test Fixture for repeated calls to a control loop. */
+class AckermannControllerTest : public ::testing::Test
+{
+ protected:
+  void SetUp() override
+  {
+    // construct our base classes;
+    opts_ = std::make_unique<PlantOptions>(1.0, 0.25, 0.2);
+
+    // construct our plant
+    plant_ = std::make_unique<Plant>(*opts_);
+
+    // construct the controller
+    controller_ = std::make_unique<Controller>();
+  }
+
+  // void TearDown() override {}
+
+  // our base class members (we use pointers to avoid default construction)
+  std::unique_ptr<PlantOptions> opts_;
+  std::unique_ptr<Plant> plant_;
+  std::unique_ptr<Controller> controller_;
+};
+
+
 /* @brief A convenience method for executing the given controller's command against the given Plant */
-void control_loop(Plant& p,
-                  Controller& c,
+void control_loop(std::unique_ptr<Plant>& p,
+                  std::unique_ptr<Controller>& c,
                   double desired_speed,
                   double desired_heading,
                   double dt,
-                  double duration)
+                  int max_iterations)
 {
-  double t = 0;
-  while (t < duration)
+  int i = 0;
+  while (i != max_iterations)
   {
     double current_speed, current_heading;
-    p.getState(current_speed, current_heading);
+    p->getState(current_speed, current_heading);
 
     // calculate required command signal
     double w_l, w_r;
-    c.update(current_speed, current_heading, desired_speed, desired_heading, w_l, w_r);
+    c->update(current_speed, current_heading, desired_speed, desired_heading, w_l, w_r);
 
     // actually apply the command
-    p.applyCommand(w_l, w_r, dt);
+    p->applyCommand(w_l, w_r, dt);
 
     // update time
-    t += dt;
+    ++i;
   }
 }
 
-// construct fake plant
-PlantOptions opts = PlantOptions(1.0, 0.25, 0.2);
-Plant plant(opts);
-
-// construct Controller and set appropriate parameters
-Controller controller;
-
 /* @brief Test that we've set up the Mock class properly. */
-TEST(System_FakeSetup, should_pass) {
+TEST_F(AckermannControllerTest, System_FakeSetup) {
   // try setting and getting the same state
   double speed_in = 1.3;
   double heading_in = 54.0;
 
-  plant.setState(speed_in, heading_in);
+  plant_->setState(speed_in, heading_in);
 
   double speed_out, heading_out;
-  plant.getState(speed_out, heading_out);
+  plant_->getState(speed_out, heading_out);
   EXPECT_EQ(speed_out, speed_in);
   EXPECT_EQ(heading_out, heading_in);
 
   // reset Plant and try setting a zero command (should result in zero state)
-  plant.setState(0.0, 0.0);
-  plant.applyCommand(0.0, 0.0, 0.0);
-  plant.getState(speed_out, heading_out);
+  plant_->setState(0.0, 0.0);
+  plant_->applyCommand(0.0, 0.0, 0.0);
+  plant_->getState(speed_out, heading_out);
   EXPECT_EQ(speed_out, 0.0);
   EXPECT_EQ(heading_out, 0.0);
 }
 
 /* @brief Test that the system converges to a desired setpoint w/ a zero noise Mock Plant. */
-TEST(System_Convergence1, should_pass) {
+TEST_F(AckermannControllerTest, System_Convergence1) {
   double desired_heading = 45.0;
   double desired_speed = 3.0;
 
-  // reset plant
-  plant.setState(0.0, 0.0);
-
   // perform control loop
-  control_loop(plant, controller, desired_speed, desired_heading, 0.1, 10.0);
+  control_loop(plant_, controller_, desired_speed, desired_heading, 0.1, 0.2);
 
   // get final state
   double final_speed, final_heading;
-  plant.getState(final_speed, final_heading);
+  plant_->getState(final_speed, final_heading);
 
   // check that we achieved our desired state
   EXPECT_EQ(final_speed, desired_speed);
@@ -88,19 +105,16 @@ TEST(System_Convergence1, should_pass) {
 }
 
 /* @brief Test that the system converges to a desired setpoint w/ a low noise Mock Plant. */
-TEST(System_Convergence2, should_pass) {
+TEST_F(AckermannControllerTest, System_Convergence2) {
   double desired_heading = -45.0;
   double desired_speed = 1.01;
 
-  // reset plant
-  plant.setState(0.0, 0.0);
-
   // perform control loop
-  control_loop(plant, controller, desired_speed, desired_heading, 0.1, 10.0);
+  control_loop(plant_, controller_, desired_speed, desired_heading, 0.1, 0.2);
 
   // get final state
   double final_speed, final_heading;
-  plant.getState(final_speed, final_heading);
+  plant_->getState(final_speed, final_heading);
 
   // check that we achieved our desired state
   EXPECT_EQ(final_speed, desired_speed);
@@ -108,19 +122,16 @@ TEST(System_Convergence2, should_pass) {
 }
 
 /* @brief Test that the system fails to converge to a "broken" Mock Plant. */
-TEST(System_NoConvergence, should_pass) {
+TEST_F(AckermannControllerTest, System_NoConvergence) {
   double desired_heading = 135.0;
   double desired_speed = 100.0;
 
-  // reset plant
-  plant.setState(0.0, 0.0);
-
   // perform control loop
-  control_loop(plant, controller, desired_speed, desired_heading, 0.1, 10.0);
+  control_loop(plant_, controller_, desired_speed, desired_heading, 0.1, 0.2);
 
   // get final state
   double final_speed, final_heading;
-  plant.getState(final_speed, final_heading);
+  plant_->getState(final_speed, final_heading);
 
   // check that we achieved our desired state
   EXPECT_EQ(final_speed, desired_speed);
