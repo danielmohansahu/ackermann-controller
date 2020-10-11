3fa6d7cecd8ddf47f80603edc2657f52d82d84c4
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-11 12:04:06 -0400

    [Task1.5] Plant Model Error Correction.
    [test/fake/plant.cpp] Discovered (and fixed) a bug in the Ackermann model. Also correctly setting speed to 0.0 for commands of 0.0.

--- test/fake/plant.cpp
+++ test/fake/plant.cpp
@@ -23,8 +23,12 @@ void Plant::applyCommand(double w_left, double w_right, double dt)
   // @TODO add noise
 
   // check for no motion (which resets speed)
-  if (w_left == 0.0 && w_right == 0.0)
+  double eps = std::numeric_limits<double>::epsilon();
+  if (std::abs(w_left) < eps && std::abs(w_right) < eps)
+  {
+    speed_ = 0.0;
     return;
+  }
 
   // simulate the application of the given commanded wheel velocities
   double w_o, w_i;
@@ -38,25 +42,22 @@ void Plant::applyCommand(double w_left, double w_right, double dt)
     w_i = w_left;
     w_o = w_right;
   }
-  
+
   // limit based on max_steering_angle; model this as wheel slip on faster (outer) wheel
-  // @TODO Daniel Sahu include derivation?
   // @TODO check for unreal results and if roots are both valid (??)
-  double alpha = std::pow(w_i / w_o, 2);
-  double r_1 = 0.5 * opts_.wheel_separation * (alpha + 1)/(alpha - 1);
-  double r_2 = 0.5 * std::sqrt(std::pow(opts_.wheel_separation, 2) * ((alpha + 1)/(alpha - 1) - 1) - 4 * std::pow(opts_.wheel_separation, 2));
+  double alpha = (w_i * w_i + w_o * w_o) / (w_o * w_o - w_i * w_i);
+  double r_1 = 0.5 * opts_.wheel_separation * alpha;
+  double r_2 = 0.5 * std::sqrt(std::pow(opts_.wheel_separation, 2.0) * (alpha * alpha - 1) - 4 * std::pow(opts_.wheel_base, 2.0));
   double r = r_1 + r_2;
 
   // check if we've violated the maximum steering angle
   //  note that we only need to check the inner wheel steering angle
   double steering_angle_i = std::atan(opts_.wheel_base / (r - 0.5 * opts_.wheel_separation));
   if (std::abs(steering_angle_i) > opts_.max_steering_angle * M_PI/180.)
-  {
     // @TODO actually do something here to limit the results
-    std::cerr << "Violated max steering angle: " << steering_angle_i << " vs " << opts_.max_steering_angle * M_PI/180. << std::endl;
-  }
+    std::cerr << "Violated max steering angle: " << steering_angle_i << " vs " << opts_.max_steering_angle * M_PI / 180. << std::endl;
 
-  // calculate speed
+  // calculate speed (instantaneously achieved)
   speed_ = M_PI * opts_.wheel_radius * (w_o + w_i);
   
   // calculate resulting heading by integrating across dt
--- test/fake/plant.h
+++ test/fake/plant.h
@@ -2,6 +2,7 @@
  */
 
 #include <iostream>
+#include <limits>
 #include <cmath>
 #include <random>
 #include <limits>
