489e8bf94ab54819aacc4390dbde76d49ea1cc6f
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-10-17 11:26:21 -0400

    [Task2.3] Modified core controller class per feedback; revamped API.

--- include/Controller.hpp
+++ include/Controller.hpp
@@ -10,9 +10,10 @@
  */
 
 #include <memory>
+#include <thread>
 
-#include "State.hpp"
-#include "AckermannVehicle.hpp"
+#include "Params.hpp"
+#include "Controller.hpp"
 #include "PID.hpp"
 #include "Limits.hpp"
 
@@ -20,20 +21,73 @@ namespace ackermann {
 
 class Controller {
  public:
-  /* @brief constructor */
-  Controller();
+  /* @brief Constructor; constructs and initializes parameters of all composition classes. */
+  explicit Controller(const Params& params);
 
-  /* @brief Calculate the appropriate wheel velocities for the given setpoints.
+  /* @brief Begin execution of a control loop. */
+  void start();
+
+  /* @brief Stop execution of a control loop and rejoin.
+   *
+   * This also calls reset() to clear any state variables.
    */
-  // @TODO [Daniel Sahu] figure out a better interface API for this method.
-  void update(double current_speed,
-              double current_heading,
-              double desired_speed,
-              double desired_heading,
-              double& left,
-              double& right);
+  void stop();
+
+  /* @brief Clear system state variables. */
+  void reset();
+
+  /* @brief Set the current state (speed, heading) of the system. 
+   * 
+   * This represents an update from external sensors as to our true
+   * system state. If this is not called the controller will continue
+   * open loop.
+   * 
+   * @param heading: The actual vehicle heading.
+   * @param speed: The actual vehicle speed.
+   */
+  void setState(const double heading, const double speed);
+
+  /* @brief Get the current state (speed, heading) of the system. 
+   * 
+   * @param heading: The estimated vehicle heading.
+   * @param speed: The estimated vehicle speed.
+   */
+  void getState(double& heading, double& speed) const;
+
+  /* @brief Set the current system setpoint (speed, heading). 
+   * 
+   * This sets a new target for our control loop. Expected usage
+   * is to call this method before calling 'start', although
+   * concurrent execution is supported.
+   * 
+   * @param heading: The desired vehicle heading.
+   * @param speed: The desired vehicle speed.
+   */
+  void setGoal(const double heading, const double speed);
+
+  /* @brief Get the current system setpoint (speed, heading). 
+   * 
+   * @param heading: The current vehicle heading setpoint.
+   * @param speed: The current vehicle speed setpoint.
+   */
+  void getGoal(double& heading, double& speed) const;
+
+  /* @brief Get the current system command (speed, heading). 
+   * 
+   * This represents the main point of access for a polling
+   * architecture, i.e. an external control loop would 
+   * periodically update the controller with the true system state
+   * and request command updates.
+   * 
+   * @param throttle: The latest throttle command (limited between [0,1]).
+   * @param steering: The latest steering angle command (degrees).
+   */
+  void getCommand(double& throttle, double& steering) const;
 
  private:
+  /* @brief Control loop (executed asynchronously) */
+  void controlLoop();
+
   /* @brief Ackermann model (used in translating 
    * speed/heading into wheel speeds)
    */
@@ -44,8 +98,14 @@ class Controller {
    */
   std::unique_ptr<Limits> limits_;
 
-  /* @brief Internal encapsulated PID controller */
-  std::unique_ptr<PID> pid_;
+  /* @brief Internal encapsulated PID controller for system speed. */
+  std::unique_ptr<PID> pid_speed_;
+
+  /* @brief Internal encapsulated PID controller for system heading. */
+  std::unique_ptr<PID> pid_heading_;
+
+  /* @brief Thread handle for the currently executing control loop. */
+  std::thread control_loop_handle_;
 };
 
 } // namespace ackermann
