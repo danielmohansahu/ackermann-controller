f9bbc25eb2ecec5c14945c7f26e59f2f394df8b0
selyard <spencer.elyard@gmail.com>
2020-10-29 18:03:10 -0400

    [Task 2.2c] Initial implemenation of Ackermann steering model.

--- app/Model.cpp
+++ app/Model.cpp
@@ -6,12 +6,14 @@
  * @author Daniel M. Sahu
  *
  * @copyright [2020]
- * 
+ *
  * http://www.iaeng.org/publication/WCE2016/WCE2016_pp1062-1066.pdf
  * https://www.xarg.org/book/kinematics/ackerman-steering/
  */
 
 #include <Model.hpp>
+#include <math.h>
+#include <atomic>
 
 namespace ackermann {
 
@@ -21,35 +23,50 @@ Model::Model(const std::shared_ptr<const Params>& params)
 }
 
 void Model::reset() {
-
+  desired_speed_ = 0.0;
+  desired_heading_ = 0.0;
+  current_speed_ = 0.0;
+  current_heading_ = 0.0;
+  current_throttle_ = 0.0;
+  current_steering_ = 0.0;
 }
 
 void Model::setState(const double speed, const double heading) {
-
+  current_speed_ = speed;
+  current_heading_ = heading;
 }
 
 void Model::getState(double& speed, double& heading) const {
-
+  speed = current_speed_;
+  heading = current_heading_;
 }
 
 void Model::setGoal(const double speed, const double heading) {
-
+  desired_speed_ = speed;
+  desired_heading_ = heading;
 }
 
 void Model::getGoal(double& speed, double& heading) const {
-
+  speed = desired_speed_;
+  heading = desired_heading_;
 }
 
 void Model::getCommand(double& throttle, double& steering) const {
-
+  throttle = current_throttle_;
+  steering = current_steering_;
 }
 
-void Model::command(const double throttle, const double steering, const double dt) const {
+void Model::command(const double throttle, const double steering, const double dt) {
+  current_heading_ = current_heading_ + (throttle/((*params_).wheel_base)) * cos(steering) * dt;
 
+  if (throttle > 0) {current_speed_ = throttle * (*params_).velocity_max;}
+  if (throttle < 0) {current_speed_ = throttle * (*params_).velocity_min;}
+  if (throttle == 0) {current_speed_ = 0;}
 }
 
 void Model::getError(double& speed_error, double& heading_error) const {
-
+  speed_error = desired_speed_ - current_speed_;
+  heading_error = desired_heading_ - current_heading_;
 }
 
 } // namespace ackermann
--- include/Model.hpp
+++ include/Model.hpp
@@ -12,6 +12,7 @@
 #include <atomic>
 
 #include "Params.hpp"
+#include "Limits.hpp"
 
 namespace ackermann {
 
@@ -26,58 +27,64 @@ class Model {
   void reset();
 
   /* @brief Set the current system state.
-   * 
+   *
    * @param speed: Actual system speed.
    * @param heading: Actual system heading.
    */
   void setState(const double speed, const double heading);
 
   /* @brief Get the current state estimate.
-   * 
+   *
    * @param speed: Estimated system speed.
    * @param heading: Estimated system heading.
    */
   void getState(double& speed, double& heading) const;
 
   /* @brief Get the target setpoint.
-   * 
+   *
    * @param speed: Desired system speed.
    * @param heading: Desired system heading.
    */
   void setGoal(const double speed, const double heading);
 
   /* @brief Get the current setpoint.
-   * 
+   *
    * @param speed: Desired system speed.
    * @param heading: Desired system heading.
    */
   void getGoal(double& speed, double& heading) const;
 
   /* @brief Get the current commanded throttle and steering angle.
-   * 
+   *
    * @param throttle: The last commanded throttle (limited to [0,1]).
    * @param steering: The last commanded steering angle (degrees).
    */
   void getCommand(double& throttle, double& steering) const;
 
   /* @brief Simulate execution of the given throttle and steering commands.
-   * 
+   *
    * @param throttle: The commanded throttle.
    * @param steering: The commanded steering angle.
    * @param dt: The amount of time to simulate over.
    */
-  void command(const double throttle, const double steering, const double dt) const;
+  void command(const double throttle, const double steering, const double dt);
 
   /* @brief Return the current error between desired and setpoint.
-   * 
+   *
    * @param speed_error: Current speed error.
    * @param heading_error: Current heading error.
    */
   void getError(double& speed_error, double& heading_error) const;
 
  private:
-  // shared parameter object (contains system kinematics)
+   /* @brief shared parameter object (contains system kinematics)
+    */
   const std::shared_ptr<const Params> params_;
+
+  /* @brief Object used to apply kinematic constraints to
+   * a calculated command (to prevent saturation)
+   */
+  std::unique_ptr<Limits> limits_;
 
   // system state variables
   std::atomic<double> desired_speed_ {0.0};
