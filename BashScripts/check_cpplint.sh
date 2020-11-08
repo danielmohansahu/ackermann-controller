6bbeea600ecb6b75b018a4a2b539f4da3e0adab6
selyard <spencer.elyard@gmail.com>
2020-11-07 21:53:34 -0500

    [Task 2.5] Update script for CPPCheck and CPPLint

--- BashScripts/check_cpplint.sh
+++ BashScripts/check_cpplint.sh
@@ -1,1 +1,1 @@
-cpplint $( find ../. -name \*.hpp -or -name \*.cpp -or -name \*.h | grep -vE -e "^./build/" -e "^./vendor/" -e "^./docs/" -e "^./doxygen/" )
+cpplint $( find ../. -name \*.hpp -or -name \*.cpp -or -name \*.h | grep -vE -e "^.././BashScripts" -e "^.././build" -e "^.././cmake" -e "^.././docs" -e "^.././doxygen" -e "^.././vendor" )
