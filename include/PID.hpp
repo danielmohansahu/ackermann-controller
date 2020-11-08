8aefe9fe64403b32e97f6b2abe5726702a96eed1
selyard <spencer.elyard@gmail.com>
2020-11-07 20:59:06 -0500

    [Task 2.6] Update doxygen documentation for PID.hpp

--- include/PID.hpp
+++ include/PID.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-/* @file PID.hpp
+/**
+ * @file PID.hpp
  * @brief Class declaration for a PID controller
  *
  * @author Spencer Elyard
@@ -15,62 +16,84 @@
 
 namespace ackermann {
 
+/**
+* @brief This class implements a PID controller with max/min clamping of output
+* values to prevent integral windup.
+*/
 class PID {
  public:
-  /* @brief constructor
-   * @parameter kP Proportional Gain
-   * @parameter kI Integral Gain
-   * @parameter kD Derivative Gain
+  /**
+  * @brief Constructor
+   * @param params Shared PIDParams pointer detailing PID parameters
+   * (kP, kI, kD)
+   * @param out_minLimit Optional, clamp minimum value of PID controller
+   * output
+   * @param out_maxLimit Optional, clamp maximum value of PID controller
+   * output
    */
-  PID(const std::shared_ptr<const PIDParams>& params, 
+  PID(const std::shared_ptr<const PIDParams>& params,
       double out_minLimit = std::numeric_limits<double>::lowest(),
       double out_maxLimit = std::numeric_limits<double>::max());
 
-  /* @brief Get the kP Proportional Gain
-   * @parameter None
+  /**
+  * @brief Get the kP Proportional Gain
+   * @param None
    * @return Proportional Gain
    */
   double get_k_p() const;
 
-  /* @brief Get the kI Integral Gain
-   * @parameter None
+  /**
+  * @brief Get the kI Integral Gain
+   * @param None
    * @return Proportional Gain
    */
   double get_k_i() const;
 
-  /* @brief Get the kD Derivative Gain
-   * @parameter None
+  /**
+  *  @brief Get the kD Derivative Gain
+   * @param None
    * @return Derivative Gain
    */
   double get_k_d() const;
 
-  /* @brief Perform PID Calculation
-   * @parameter current_error Current Error(Feedback)
-   * @parameter dt time
+  /** @brief Perform PID Calculation
+   * @param current_error Current Error (Feedback)
+   * @param dt Change in time since previous value collected.
    * @return Output
    */
   double getCommand(double current_error, double dt);
 
-  /* @brief Reset the PID
-   * @parameter None
+  /**
+  * @brief Reset the PID
+   * @param None
    * @return None
    */
   void reset_PID();
 
  private:
-  // @brief PID Gains (kp, ki, kd)
+  /**
+  * @brief PID Gains (kp, ki, kd)
+  */
   const std::shared_ptr<const PIDParams> params_;
 
-  // @brief Previous Error
+  /**
+  * @brief Previous Error
+  */
   double prev_error_;
 
-  // @brief Integral Error
+  /**
+  * @brief Integral Error
+  */
   double integral_error_;
 
-  //@brief Output Minimum Limit (For PID windup)
+  /**
+  * @brief Output Minimum Limit (For PID windup)
+  */
   double out_minLimit_;
 
-  // @brief Output Maximum Limit (For PID windup)
+  /**
+  * @brief Output Maximum Limit (For PID windup)
+  */
   double out_maxLimit_;
 };
 
